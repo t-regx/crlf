945824fcbff9c5b303c769579cf6c8825f17f699
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-02-08 01:27:23 +0100

    Clean, extract convert()

--- crlf/crlf.py
+++ crlf/crlf.py
@@ -6,7 +6,11 @@ def main(base: str, arguments: list[str]) -> None:
     parser = ArgumentParser('crlf', description='Tool to change line endings of text files')
     parser.add_argument('filename', help='file or directory')
     args = parser.parse_args(arguments)
-    if isfile(join(base, args.filename)):
-        print(f'Corrected file {args.filename}')
+    convert(parser, base, args.filename)
+
+
+def convert(parser, base: str, filename: str) -> None:
+    if isfile(join(base, filename)):
+        print(f'Corrected file {filename}')
     else:
-        parser.error(f"file does not exist '{args.filename}'")
+        parser.error(f"file does not exist '{filename}'")
