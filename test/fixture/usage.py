c88599ebc979d9d586b5141ee661c68d85aa9ff4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-02-19 14:23:09 +0100

    Clean, simplify test.fixture.usage functions

--- test/fixture/usage.py
+++ test/fixture/usage.py
@@ -12,7 +12,7 @@ def summary(updated: int = 0, ignored: int = 0, failed: int = 0, dryrun: bool = 
 
 def updated(filenames: list[str], dryrun: bool = False) -> str:
     return append_dryrun(
-        ''.join([f"Updated: {filename}\n" for filename in filenames]) + summary(updated=len(filenames)),
+        ''.join([_updated(filename) for filename in filenames]) + summary(updated=len(filenames)),
         dryrun)
 
 
@@ -34,22 +34,24 @@ def failed(filenames: list[str], dryrun: bool = False) -> str:
         dryrun)
 
 
+def _updated(filename: str) -> str:
+    return f"Updated: {filename}\n"
+
+
 def _ignored(filename: str, destination: str) -> str:
-    return f"""Ignored: {filename}
-         ^ file already has {destination.upper()} line endings
-"""
+    return _format('Ignored:', filename, f'^ file already has {destination.upper()} line endings')
 
 
 def _failed(filename: str) -> str:
-    return f"""Failed:  {filename}
-         ^ ! expected text file in unicode encoding, failed to parse file
-"""
+    return _format('Failed:', filename, '^ ! expected text file in unicode encoding, failed to parse file')
 
 
 def _restricted(filename: str) -> str:
-    return f"""Failed:  {filename}
-         ^ insufficient permissions to open file
-"""
+    return _format('Failed:', filename, '^ insufficient permissions to open file')
+
+
+def _format(type: str, filename: str, message: str) -> str:
+    return type.ljust(9) + filename + "\n" + ' ' * 9 + message + "\n"
 
 
 def append_dryrun(content: str, dryrun: bool) -> str:
